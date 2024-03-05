59d7cf122c8deaf260ab1cf0adb7b5eea36c8484
Kamil Ogórek <kamil.ogorek@gmail.com>
2024-03-05 17:09:40 +0100

    jokes views

---
+++ supabase/seed.sql
@@ -0,0 +1,6 @@
+insert into public.jokes (created_at, content, rating, views) values
+  (now(), 'Why did the scarecrow win an award? Because he was outstanding in his field!', 5, 60),
+  (now(), 'I''m reading a book on anti-gravity. It''s impossible to put down!', 4, 42),
+  (now(), 'Parallel lines have so much in common. It’s a shame they’ll never meet.', 3, 1338),
+  (now(), 'I told my wife she should embrace her mistakes. She gave me a hug.', 4, 10),
+  (now(), 'I''m reading a book on the history of glue. I just can''t seem to put it down.', 5, 2);
