37aeb918de433e0736c55eebcdd14a919ccb06fc
Kamil Ogórek <kamil.ogorek@gmail.com>
2024-05-28 10:02:56 +0200

    Update seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,5 +1,5 @@
 insert into public.jokes (created_at, content, rating, views) values
-  (now(), 'Why did the scarecrow win an award? Because he was outstanding in his field!', 5, 60),
+  (now(), 'Why did the scarecrow win an award? Because he was outstanding in his field!', 3, 60),
   (now(), 'I''m reading a book on anti-gravity. It''s impossible to put down!', 4, 42),
   (now(), 'Parallel lines have so much in common. It’s a shame they’ll never meet.', 3, 1338),
   (now(), 'I told my wife she should embrace her mistakes. She gave me a hug.', 4, 10),
